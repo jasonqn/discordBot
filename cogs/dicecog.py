1242f65afc0688b10e87883fea234fc3a0ea5459
Jasonqn <jasonqn94@gmail.com>
2024-05-01 22:01:09 +0100

    Multi dice command implemented

--- cogs/dicecog.py
+++ cogs/dicecog.py
@@ -9,11 +9,27 @@ from responses import *
 # the Dice class
 def roll_dice(select_dice: int, die_face_selection: int):
     # how many faces each die will have
-    die_face = [4, 6, 8, 10, 12, 20, 100]
+    die_face = {
+        4: 4,
+        6: 6,
+        8: 8,
+        10: 10,
+        12: 12,
+        20: 20,
+        100: 100
+    }
+
     # amount of dice users can select, max 10
     dice_amount = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
     # roll logic
-    dice_roll = select_dice * (randint(1, die_face_selection))
+    dice_rolls = [randint(1, die_face[die_face_selection]) for _ in range(select_dice)]
+
+    # total rolls
+    total_roll = sum(dice_rolls)
+
+    # Format individual rolls
+    individual_rolls = ', '.join(str(roll) for roll in dice_rolls)
 
     # error handling
     if select_dice not in dice_amount:
@@ -22,8 +38,7 @@ def roll_dice(select_dice: int, die_face_selection: int):
     if die_face_selection not in die_face:
         return 'Invalid dice selection, only 4, 6, 8, 10, 12, 20, 100 sided die can be selected'
 
-    # successful output
-    return f'You chose {select_dice}  dice with {die_face_selection} faces giving you {dice_roll}'
+    return f'You chose {select_dice}  dice with {die_face_selection} faces: {individual_rolls}. Total: {total_roll}'
 
 
 class DiceCog(commands.Cog):
