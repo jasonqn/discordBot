ebe65e8857690f1bf4cbc5714bd1d14ca68cb80d
Jasonqn <jasonqn94@gmail.com>
2024-05-12 22:39:39 +0100

    Buttons functioning with embed message

--- cogs/mongodb.py
+++ cogs/mongodb.py
@@ -17,14 +17,13 @@ class PlayerRegistration(commands.Cog):
 
     @commands.command(name="register")
     async def register(self, ctx):
-
-        yes = discord.ui.Button(emoji=discord.PartialEmoji.from_str("<:ballot_box_with_check:>"))
-        no = discord.ui.Button(emoji=discord.PartialEmoji.from_str("<:regional_indicator_x:>"))
-        embed_register = discord.Embed(title="Account Registration",
-                                       description=f"Would you like to register an account with DnD Bot? ")
-
         try:
-            await ctx.send(embed=embed_register)
+            view = Buttons()
+
+            embed_register = discord.Embed(title="Account Registration",
+                                           description=f"Would you like to register an account with DnD Bot? ")
+
+            await ctx.send(embed=embed_register, view=view)
         except Exception as e:
             print(e)
 
@@ -36,30 +35,20 @@ class Buttons(discord.ui.View):
 
         self.registered_users = []
 
-    @discord.ui.button(label="Yes", style=discord.ButtonStyle.success,
-                       emoji=discord.PartialEmoji.from_str("<:ballot_box_with_check:>"))
+    @discord.ui.button(label="Yes", style=discord.ButtonStyle.green)
     async def yes_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         # checks if the user has already registered
         if interaction.user in self.registered_users:
             return await (interaction.response.send_message
-                          (content="User already    registered!", ephemeral=True))
+                          (content="User already registered!", ephemeral=True))
 
-        self.registered_users.append(interaction.user)  # add the user to voted list
+        self.registered_users.append(interaction.user)  # add the user to register list
 
-    @discord.ui.button(label="No", style=discord.ButtonStyle.danger,
-                       emoji=discord.PartialEmoji.from_str("<:regional_indicator_x:>"))
-    async def no_button(
-            self, interaction: discord.Interaction, button: discord.ui.Button
-    ):
-        if (
-                interaction.user in self.registered_users
-        ):  # check if the user has already voted or not and return if true
-            return await interaction.response.send_message(
-                content="User already registered!", ephemeral=True
-            )
+    @discord.ui.button(label="No", style=discord.ButtonStyle.red)
+    async def no_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if interaction.user in self.registered_users:
+            return await interaction.response.send_message(content="User already registered!", ephemeral=True)
         return
-
-
 
 
 async def setup(bot):
