c367fea8a349dca57a0dce90addf5f422aef9a4f
Jasonqn <jasonqn94@gmail.com>
2024-05-22 22:01:21 +0100

    added dice roll verification by checking if user is registered

--- cogs/dicecog.py
+++ cogs/dicecog.py
@@ -1,9 +1,15 @@
 from random import randint
-
 import discord
 from discord.ext import commands
-
 from discord.ext.commands import Cog
+
+import config
+
+# import config class for database
+clientObj = config.Oauth()
+client = clientObj.databaseCONN()
+db = client.dnd
+collection = db.dice_rolls
 
 
 # dice roll function called into "roll" function below within
@@ -45,12 +51,26 @@ def roll_dice(select_dice: int, die_face_selection: int):
 class DiceCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.collection = collection
 
     @commands.command(name='roll')
     async def roll(self, ctx, select_dice: int, die_face_selection: int):
-        print("Roll command call")
+        user_id = str(ctx.author.id)
+        user = db.login.find_one({"user_id": user_id})
+
+        if not user:
+            await ctx.send("You are not registered. Please register first using the !register command.")
+            return
+
         try:
             response = roll_dice(select_dice, die_face_selection)
+            roll_entry = {
+                "user_id": user_id,
+                "username": ctx.author.name,
+                "rolls": response
+            }
+            self.collection.insert_one(roll_entry)
+
             await ctx.send(response)
         except Exception as e:
             print(e)
