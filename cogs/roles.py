3fe75baab84742593ff4bbe8609e5086c0f9241e
Jasonqn <jasonqn94@gmail.com>
2024-05-23 22:16:27 +0100

    created init function and emojis for each class

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,4 +1,6 @@
+import discord
 from discord.ext import commands
+from discord import Intents, Message, Reaction, User, Guild, RawReactionActionEvent
 from discord.ext.commands import Cog
 import config
 
@@ -8,11 +10,28 @@ client = clientObj.databaseCONN()
 db = client.dnd
 
 
-class ReactionRoles(commands.Cog):
+class ReactionRoles(Cog):
 
     def __init__(self, bot):
+        super().__init__()
         self.bot = bot
         self.db = db
+        self.role_emojis = {
+            ':shield:': 'Paladin',
+            ':crossed_swords:': 'Fighter',
+            ':axe:': 'Barbarian',
+            ':violin:': 'Bard',
+            ':sparkling_heart:': 'Cleric',
+            ':bear:': 'Druid',
+            ':punch:': 'Monk',
+            ':archery:': 'Ranger',
+            ':dagger:': 'Rogue',
+            ':zap:': 'Sorcerer',
+            ':mage:': 'Warlock',
+            ':fire:': 'Wizard'
+        }
+
+    async def on_reaction_add(self, payload: RawReactionActionEvent):
+        payload.message_id in self.role_emojis.keys()
 
 
-    def
