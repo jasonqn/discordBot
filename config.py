fd0d348c8fbe1f2df2ee4462bf1893798cf55233
Jasonqn <jasonqn94@gmail.com>
2024-07-17 21:20:13 +0100

    setup database connection, need to make this into a class that can be retrieved by cogs

--- config.py
+++ config.py
@@ -1,24 +1,35 @@
 import os
 from typing import Final
-import pymongo
 from dotenv import load_dotenv
-
+import psycopg
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-DB_LINK: Final[str] = os.getenv('MONGO_DB_CONN')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')  # Provide default port 5432
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+
+connection_string = (f"dbname={DATABASE_NAME} user={DATABASE_USERNAME} password={DATABASE_PASSWORD} host={DATABASE_HOST}"
+                     f" port={DATABASE_PORT}")
+
+try:
+    connection = psycopg.connect(connection_string)
+    cursor = connection.cursor()
+    print("Connection to PostgreSQL DB successful")
+except Exception as error:
+    print(f"Error while connecting to PostgreSQL: {error}")
 
 
 class Oauth:
     def __init__(self):
-        self.TOKEN = TOKEN
+        self.token = TOKEN
         self.OWNER_IDS = ['627569347417866271']
-        self.db_link = DB_LINK
-        self.client = pymongo.MongoClient(self.db_link)
+        self.connection = connection
 
     def discordTOKEN(self):
-        return self.TOKEN, self.OWNER_IDS
+        return self.token, self.OWNER_IDS
 
     def databaseCONN(self):
-        return self.client
-
+        return self.connection
