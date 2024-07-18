0a85b2a5d9f4d941968a18f0854fa8f99854f19f
Jasonqn <jasonqn94@gmail.com>
2024-07-18 23:00:54 +0100

    created connection class to database and configed db tables

--- config.py
+++ config.py
@@ -11,25 +11,23 @@ DATABASE_HOST = os.getenv('DATABASE_HOST')
 DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')  # Provide default port 5432
 DATABASE_NAME = os.getenv('DATABASE_NAME')
 
-connection_string = (f"dbname={DATABASE_NAME} user={DATABASE_USERNAME} password={DATABASE_PASSWORD} host={DATABASE_HOST}"
-                     f" port={DATABASE_PORT}")
-
-try:
-    connection = psycopg.connect(connection_string)
-    cursor = connection.cursor()
-    print("Connection to PostgreSQL DB successful")
-except Exception as error:
-    print(f"Error while connecting to PostgreSQL: {error}")
+connection_string = (
+    f"dbname={DATABASE_NAME} user={DATABASE_USERNAME} password={DATABASE_PASSWORD} host={DATABASE_HOST}"
+    f" port={DATABASE_PORT}")
 
 
 class Oauth:
     def __init__(self):
         self.token = TOKEN
         self.OWNER_IDS = ['627569347417866271']
-        self.connection = connection
 
     def discordTOKEN(self):
         return self.token, self.OWNER_IDS
 
     def databaseCONN(self):
-        return self.connection
+        try:
+            connection = psycopg.connect(connection_string)
+            cursor = connection.cursor()
+            print("Connection to PostgreSQL DB successful")
+        except Exception as error:
+            print(f"Error while connecting to PostgreSQL: {error}")
